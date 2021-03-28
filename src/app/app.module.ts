710497d62a408522570259e70af34d159674613e
Ruben Cristea <rubendcristea@gmail.com>
2021-03-27 23:09:03 -0400

    Main image added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { MainImageComponent } from './components/main-image/main-image.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    MainImageComponent,
   ],
   imports: [
     BrowserModule,
