79e1eb48b927fce9295bd5e2a85995101ab13270
Ruben Cristea <rubendcristea@gmail.com>
2021-04-03 10:11:05 -0400

    Updated floors page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/pages/about-us/about-us.component';
+import { FloorsComponent } from './components/pages/floors/floors.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'about-us', component: AboutUsComponent },
+  { path: 'floors', component: FloorsComponent },
 ];
 
 @NgModule({
