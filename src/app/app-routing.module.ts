6b22df68937da48e782611105f295bcfb1eb345a
Ruben Cristea <39315168+rcristea@users.noreply.github.com>
2021-04-10 08:44:16 -0400

    Revert "Feature engineered"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutUsComponent } from './components/pages/about-us/about-us.component';
-import { EngineeredComponent } from './components/pages/engineered/engineered.component';
 import { FloorsComponent } from './components/pages/floors/floors.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
-import { ServicesComponent } from './components/pages/services/services.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'floors', component: FloorsComponent },
-  { path: 'engineered', component: EngineeredComponent },
-  { path: 'services', component: ServicesComponent },
 ];
 
 @NgModule({
