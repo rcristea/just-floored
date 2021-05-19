69c0b8db04687109f39f2acdecd40ba559abf042
Ruben Cristea <rubendcristea@gmail.com>
2021-05-19 14:23:30 -0400

    Added form validation for contact us form

--- src/app/components/snippets/contact-us/contact-us.component.ts
+++ src/app/components/snippets/contact-us/contact-us.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup, FormBuilder } from '@angular/forms';
 import { SendEmailService } from '../../../services/send-email.service';
 
 @Component({
@@ -8,29 +8,26 @@ import { SendEmailService } from '../../../services/send-email.service';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent {
-  name = new FormControl('', [
-    Validators.required,
-  ]);
-  email = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  phone = new FormControl();
-  message = new FormControl('', [
-    Validators.required,
-  ]);
+  contactUsForm: FormGroup = this.formBuilder.group({
+    name: [null, Validators.required],
+    email: [null, [Validators.required, Validators.email]],
+    phone: null,
+    message: [null, Validators.required],
+  });
 
-  constructor(public sendEmail: SendEmailService) { }
+  messageSent: boolean = false;
 
-  ngOnInit(): void {
+  constructor(public sendEmail: SendEmailService, private formBuilder: FormBuilder) { 
   }
+
+  ngOnInit(): void { }
 
   onSubmit() {
     let formData = {
-      name: this.name.value,
-      email: this.email.value,
-      phone: this.phone.value,
-      message: this.message.value,
+      name: this.contactUsForm.controls.name.value,
+      email: this.contactUsForm.controls.email.value,
+      phone: this.contactUsForm.controls.phone.value,
+      message: this.contactUsForm.controls.message.value,
     }
 
     this.sendEmail.to('http://localhost:3000/contact/sendmail', formData).subscribe(
@@ -38,6 +35,9 @@ export class ContactUsComponent {
         let res: any = data;
         console.log(`${formData.name} succesfully sent a message.`);
       }
-    )
+    );
+
+    this.contactUsForm.reset();
+    this.messageSent = true;
   }
 }
